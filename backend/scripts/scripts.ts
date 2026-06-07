94cc049f123b795e75ffa110b4c5e2a7b045f381
Nyles Geiger <nygeiger@gmail.com>
2026-06-06 22:26:56 -0400

    add logic to seed database with words

--- backend/scripts/scripts.ts
+++ backend/scripts/scripts.ts
@@ -1,5 +1,5 @@
-import { executeMigrationScripts } from "../src/db/migrate.js";
-import { db_migration_folder } from "../src/utils/constants.js";
+import { executeMigrationScripts, executeSeedScripts } from "../src/db/migrate.js";
+import { db_migration_folder, db_seeding_folder } from "../src/utils/constants.js";
 
 const taskName = process.argv[2]
 
@@ -7,6 +7,9 @@ const run = async () => {
     switch (taskName) {
         case "db_migrate":
             await executeMigrationScripts(db_migration_folder)
+            break
+            case "db_seed":
+            await executeSeedScripts(db_seeding_folder)
             break
         case "hello_world":
             console.log("Hello World")
--- backend/src/db/migrate.ts
+++ backend/src/db/migrate.ts
@@ -19,3 +19,20 @@ export async function executeMigrationScripts(folderPath: string) {
         console.error("Error reading in migration scripts:", err);
     }
 }
+
+export async function executeSeedScripts(folderPath: string) {
+    try {
+        // Returns an array of filenames in the directory
+        const files = (await readdir(folderPath)).filter(f => f.endsWith(".sql")).sort();
+        console.log("DB connection string: ", env.DATABASE_URL)
+        for (const file of files) {
+            const currentFilePath = join(folderPath, file)
+            const sql = await readFile(currentFilePath, 'utf8');
+            console.log(`Running database seed script at: ${currentFilePath}`)
+            await dbQuery(sql);
+        }
+        console.log("All seeding scripts ran successfully")
+    } catch (err) {
+        console.error("Error reading in migration scripts:", err);
+    }
+}
