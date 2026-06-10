bf96718b3bf73030951d712c70690b722b1b7498
Nyles Geiger <nygeiger@gmail.com>
2026-06-10 02:29:38 -0400

    add table to track migration scripts and only run those necessary

--- backend/src/db/migrate.ts
+++ backend/src/db/migrate.ts
@@ -5,14 +5,24 @@ import env from "../config/env.js"
 
 export async function executeMigrationScripts(folderPath: string) {
     try {
+        await dbQuery(`CREATE TABLE IF NOT EXISTS migrations (filename TEXT NOT NULL UNIQUE, run_at TIMESTAMPTZ DEFAULT NOW())`)
+        const migratedFiles = (await dbQuery<{ filename: string }>(`SELECT filename FROM migrations`)).rows.map(f => f.filename)
+
         // Returns an array of filenames in the directory
         const files = (await readdir(folderPath)).filter(f => f.endsWith(".sql")).sort();
+
         console.log("DB connection string: ", env.DATABASE_URL)
         for (const file of files) {
             const currentFilePath = join(folderPath, file)
-            const sql = await readFile(currentFilePath, 'utf8');
-            console.log(`Running database migration script at: ${currentFilePath}`)
-            await dbQuery(sql);
+            if (migratedFiles.includes(file)) {
+                continue;
+            } else {
+
+                const sql = await readFile(currentFilePath, 'utf8');
+                console.log(`Running database migration script at: ${currentFilePath}`)
+                await dbQuery(sql);
+                await dbQuery("INSERT INTO migrations (filename) VALUES ($1)", [file])
+            }
         }
         console.log("All migration scripts ran successfully")
     } catch (err) {
